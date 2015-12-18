c607e35ffb2da38a61107e9a56836897412cb1f6
Anton Ye <me@antonye.ru>
2015-12-18 05:59:44 +0300

    Create Bootstrap style app

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'turbolinks', '2.5.3'
 gem 'jbuilder', '2.3.2'
 gem 'sdoc', '0.4.1', group: :doc
 
+gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+
+  match '/help', to: 'static_pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
